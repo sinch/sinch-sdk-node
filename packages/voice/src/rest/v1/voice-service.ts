1a3745eb59deea4ef321c33ae17a648fd9168b43
Antoine Sein <antoine.sein@mailgun.com>
2024-04-05 14:53:13 +0200

    Refactor setApplicationsManagementHostname

--- packages/voice/src/rest/v1/voice-service.ts
+++ packages/voice/src/rest/v1/voice-service.ts
@@ -31,7 +31,7 @@ export class VoiceService {
    * Update the default hostname for the Applications API
    * @param {string} hostname - The new hostname to use for the Applications API.
    */
-  public setApplicationManagementHostname(hostname: string) {
+  public setApplicationsManagementHostname(hostname: string) {
     this.applications.setHostname(hostname);
   }
 
