338753872c7fb8ff0335be96d66a7a290a46347c
Antoine Sein <antoine.sein@mailgun.com>
2024-04-08 16:23:11 +0200

    Fix merge issue

--- packages/fax/src/rest/v3/fax-domain-api.ts
+++ packages/fax/src/rest/v3/fax-domain-api.ts
@@ -7,8 +7,6 @@ import {
   SinchClientParameters,
   SupportedFaxRegion,
   UnifiedCredentials,
-  buildOAuth2ApiClientOptions,
-  FaxRegion,
 } from '@sinch/sdk-client';
 
 export class FaxDomainApi implements Api {
