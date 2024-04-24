616d03c14856cec57e6c3141b0e0ee63fbd9eb76
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-04-24 18:40:46 +0200

    Remove unnecessary comments (#70)

--- packages/sdk-core/src/sinch-client.ts
+++ packages/sdk-core/src/sinch-client.ts
@@ -22,22 +22,11 @@ export class SinchClient {
    * @param {SinchClientParameters} params - The object containing the Sinch credentials.
    */
   constructor(params: SinchClientParameters) {
-    // Initialize the "Conversation" API
     this.conversation = new ConversationService(params);
-
-    // Initialize the "Fax" API
     this.fax = new FaxService(params);
-
-    // Initialize the "Numbers" API
     this.numbers = new NumbersService(params);
-
-    // Initialize the "SMS" API.
     this.sms = new SmsService(params);
-
-    // Initialize the "Verification" API
     this.verification = new VerificationService(params);
-
-    // Initialize the "Voice" API
     this.voice = new VoiceService(params);
   }
 }
