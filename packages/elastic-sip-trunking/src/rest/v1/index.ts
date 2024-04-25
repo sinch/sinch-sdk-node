a0bf85c8e818c3cc8d2b6e5adda17bb243a40bdc
Antoine Sein <antoine.sein@mailgun.com>
2024-04-25 13:52:32 +0200

    DEVEXP-413: Integrate EST API in the SinchClient

---
+++ packages/elastic-sip-trunking/src/rest/v1/index.ts
@@ -0,0 +1,1 @@
+export * from './elastic-sip-trunking-service';
--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -1,5 +1,6 @@
 export * from './sinch-client';
 export * from '@sinch/conversation';
+export * from '@sinch/elastic-sip-trunking';
 export * from '@sinch/fax';
 export * from '@sinch/numbers';
 export * from '@sinch/sms';
