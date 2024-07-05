9ff00d37b3646a810f1db17a8343226424fd1e1d
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-07-05 17:05:45 +0200

    DEVEXP-366: E2E Numbers/Webhooks (#104)

---
+++ packages/numbers/tests/rest/v1/webhooks/webhooks.steps.ts
@@ -0,0 +1,51 @@
+import { Given, Then, When } from '@cucumber/cucumber';
+import { NumbersCallbackWebhooks, Numbers } from '../../../../src';
+import assert from 'assert';
+import { IncomingHttpHeaders } from 'http';
+
+const SINCH_NUMBERS_CALLBACK_SECRET = 'strongPa$$PhraseWith36CharactersMax';
+let numbersCallbackWebhook: NumbersCallbackWebhooks;
+let rawEvent: any;
+let event: Numbers.CallbackPayload;
+let formattedHeaders: IncomingHttpHeaders;
+
+const processEvent = async (response: Response) => {
+  formattedHeaders = {};
+  response.headers.forEach((value, name) => {
+    formattedHeaders[name.toLowerCase()] = value;
+  });
+  rawEvent = await response.text();
+  rawEvent = rawEvent.replace(/\s+/g, '');
+  console.log(rawEvent);
+  event = numbersCallbackWebhook.parseEvent(JSON.parse(rawEvent));
+};
+
+Given('the Numbers Webhooks handler is available', function () {
+  numbersCallbackWebhook = new NumbersCallbackWebhooks(SINCH_NUMBERS_CALLBACK_SECRET);
+});
+
+When('I send a request to trigger the success to provision to voice platform event', async () => {
+  const response = await fetch('http://localhost:3013/webhooks/numbers/provisioning_to_voice_platform/succeeded');
+  await processEvent(response);
+});
+
+Then('the event header contains a valid signature', () => {
+  assert.ok(numbersCallbackWebhook.validateAuthenticationHeader(formattedHeaders, rawEvent));
+});
+
+Then('the event describes a success to provision to voice platform event', () => {
+  assert.equal(event.eventType, 'PROVISIONING_TO_VOICE_PLATFORM');
+  assert.equal(event.status, 'SUCCEEDED');
+  assert.equal(event.failureCode, null);
+});
+
+When('I send a request to trigger the failure to provision to voice platform event', async () => {
+  const response = await fetch('http://localhost:3013/webhooks/numbers/provisioning_to_voice_platform/failed');
+  await processEvent(response);
+});
+
+Then('the event describes a failure to provision to voice platform event', () => {
+  assert.equal(event.eventType, 'PROVISIONING_TO_VOICE_PLATFORM');
+  assert.equal(event.status, 'FAILED');
+  assert.equal(event.failureCode, 'PROVISIONING_TO_VOICE_PLATFORM_FAILED');
+});
