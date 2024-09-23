dae67649a13aaa0b73d99a2cb1a2fc18d79acf8d
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-09-23 20:46:46 +0200

    DEVEXP-525: E2E Voice/Calls (#134)

--- packages/voice/src/models/v1/participant/participant.ts
+++ packages/voice/src/models/v1/participant/participant.ts
@@ -4,7 +4,7 @@
 export interface Participant {
 
   /** The type of the participant (caller or callee). */
-  type?: string;
+  type?: 'number' | 'Number' | 'username' | 'Username' | 'sip' | 'did' | string;
   /** The phone number, user name, or other identifier of the participant (caller or callee). */
   endpoint?: string;
 }
