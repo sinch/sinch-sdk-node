7c63d9888bba021afa7d78b58137b669dce6e566
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-01-20 16:25:47 +0100

    Update service

--- packages/mailgun/src/rest/v1/emails/emails-api.ts
+++ packages/mailgun/src/rest/v1/emails/emails-api.ts
@@ -1,47 +1,27 @@
 import {
   GetStoredEmailResponse,
+  SendingQueuesStatusResponse,
   SendEmailRequest,
   SendEmailResponse,
-  SendingQueuesStatusResponse,
   SendMimeEmailRequest,
 } from '../../../models';
 import {
-  RequestBody,
-  SinchClientParameters,
-  MailgunStorageHostname,
-} from '@sinch/sdk-client';
-import { MailgunDomainApi } from '../mailgun-domain-api';
-import {
-  transformSendEmailRequestIntoApiRequestBody,
-} from '../../../models/v1/emails/request/send-email-request/send-email-request.transform';
-import {
-  transformSendMimeEmailRequestIntoApiRequestBody,
-} from '../../../models/v1/emails/request/send-mime-email-request/send-mime-email-request.transform';
-import {
-  transformSendEmailResponseIntoClientResponse,
-} from '../../../models/v1/emails/response/send-email-response/send-email-response.transform';
-import {
-  transformGetEmailResponseIntoClientResponse,
-} from '../../../models/v1/emails/response/get-email-response/get-stored-email-response.transform';
-import {
-  transformSendingQueuesStatusResponseIntoClientResponse,
-} from '../../../models/v1/emails/response/sending-queues-status-response/sending-queues-status-response.transform';
-import {
-  SendEmailResponseFromApi,
-} from '../../../models/v1/emails/response/send-email-response/send-email-response';
-import {
   GetStoredEmailResponseFromApi,
-} from '../../../models/v1/emails/response/get-email-response/get-stored-email-response';
-import {
-  GenericResponseFromApi,
-} from '../../../models/v1/emails/response/generic-response/generic-response';
+} from '../../../models/v1/emails/response/get-stored-email-response/get-stored-email-response';
 import {
   SendingQueuesStatusResponseFromApi,
 } from '../../../models/v1/emails/response/sending-queues-status-response/sending-queues-status-response';
+import { SendEmailResponseFromApi } from '../../../models/v1/emails/response/send-email-response/send-email-response';
+import { GenericResponseFromApi } from '../../../models/v1/response/generic-response/generic-response';
+import { MailgunStorageHostname, RequestBody, SinchClientParameters } from '@sinch/sdk-client';
+import { MailgunDomainApi } from '../mailgun-domain-api';
+import { transformSendEmailRequestIntoApiRequestBody } from '../../../models/v1/emails/request/send-email-request/send-email-request.transform';
+import { transformSendMimeEmailRequestIntoApiRequestBody } from '../../../models/v1/emails/request/send-mime-email-request/send-mime-email-request.transform';
+import { transformGetStoredEmailResponseIntoClientResponse } from '../../../models/v1/emails/response/get-stored-email-response/get-stored-email-response.transform';
+import { transformSendingQueuesStatusResponseIntoClientResponse } from '../../../models/v1/emails/response/sending-queues-status-response/sending-queues-status-response.transform';
+import { transformSendEmailResponseIntoClientResponse } from '../../../models/v1/emails/response/send-email-response/send-email-response.transform';
 
 export class EmailsApi extends MailgunDomainApi {
-  storageHostnames: string[];
-
   /**
    * Initialize your interface
    *
@@ -49,64 +29,6 @@ export class EmailsApi extends MailgunDomainApi {
    */
   constructor(sinchClientParameters: SinchClientParameters) {
     super(sinchClientParameters, 'EmailsApi');
-    this.storageHostnames = [];
-  }
-
-  /**
-   * Send an email
-   * Pass the components of the messages such as To, From, Subject, HTML and text parts, attachments, etc. Mailgun will build a MIME representation of the message and send it. Note: In order to send you must provide one of the following parameters: 'text', 'html', 'amp-html' or 'template'
-   * @param { string } domainName - Domain name used to send the message
-   * @param { SendEmailRequest } request - The data to provide to the API call.
-   */
-  public async sendEmail(domainName: string, request: SendEmailRequest): Promise<SendEmailResponse> {
-    this.client = this.getSinchClient();
-    const getParams = this.client.extractQueryParams<SendEmailRequest>(request, [] as never[]);
-    const headers: { [key: string]: string | undefined } = {
-      Accept: 'application/json',
-      'Content-Type': 'multipart/form-data; charset=utf-8',
-    };
-    const body: RequestBody = transformSendEmailRequestIntoApiRequestBody(request);
-    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/${domainName}/messages`;
-
-    const requestOptions = await this.client.prepareOptions(basePathUrl, 'POST', getParams, headers, body || undefined);
-    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
-
-    const apiResponse = await  this.client.processCall<SendEmailResponseFromApi>({
-      url,
-      requestOptions,
-      apiName: this.apiName,
-      operationId: 'sendEmail',
-    });
-
-    return transformSendEmailResponseIntoClientResponse(apiResponse);
-  }
-
-  /**
-   * Send an email in MIME format
-   * Build a MIME string yourself using a MIME library for your programming language and submit it to Mailgun.
-   * @param { string } domainName - Domain name used to send the message
-   * @param { SendMimeEmailRequest } request - The data to provide to the API call.
-   */
-  public async sendMimeEmail(domainName: string, request: SendMimeEmailRequest): Promise<SendEmailResponse> {
-    this.client = this.getSinchClient();
-    const getParams = this.client.extractQueryParams<SendMimeEmailRequest>(request, [] as never[]);
-    const headers: { [key: string]: string | undefined } = {
-      Accept: 'application/json',
-    };
-    const body: RequestBody = transformSendMimeEmailRequestIntoApiRequestBody(request);
-    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/${domainName}/messages.mime`;
-
-    const requestOptions = await this.client.prepareOptions(basePathUrl, 'POST', getParams, headers, body || undefined);
-    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
-
-    const apiResponse = await this.client.processCall<SendEmailResponseFromApi>({
-      url,
-      requestOptions,
-      apiName: this.apiName,
-      operationId: 'sendMimeEmail',
-    });
-
-    return transformSendEmailResponseIntoClientResponse(apiResponse);
   }
 
   /**
@@ -132,20 +54,108 @@ export class EmailsApi extends MailgunDomainApi {
       url,
       requestOptions,
       apiName: this.apiName,
-      operationId: 'getEmail',
+      operationId: 'getStoredEmail',
     });
 
-    return transformGetEmailResponseIntoClientResponse(apiResponse);
+    return transformGetStoredEmailResponseIntoClientResponse(apiResponse);
   }
 
   /**
-   * Delete scheduled and undelivered mail
-   * Deletes all scheduled and undelivered mail from the domain queue.  This endpoint must be called on the storage API host and in the domain's region.  e.g. https://storage-us-east4.api.mailgun.net/v3/example.com/envelopes
-   *
-   * The storage hosts are `storage-us-east4.api.mailgun.net`, `storage-us-west1.api.mailgun.net`, and `storage-europe-west1.api.mailgun.net`.
-   * @param { string } domainName - The name of the domain you want to delete envelope from
-   * @param { MailgunStorageHostname } storageHostname - The storage hostname to be purged
+   * Get messages queue status
+   * Provides default and scheduled message queue information.
+   * @param { string } name - The name of the domain you want get sending queues from
    */
+  public async getSendingQueuesStatus(name: string): Promise<SendingQueuesStatusResponse> {
+    this.client = this.getSinchClient();
+    const getParams = {};
+    const headers: { [key: string]: string | undefined } = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    };
+    const body: RequestBody = '';
+    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/domains/${name}/sending_queues`;
+
+    const requestOptions = await this.client.prepareOptions(basePathUrl, 'GET', getParams, headers, body || undefined);
+    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
+
+    const apiResponse = await this.client.processCall<SendingQueuesStatusResponseFromApi>({
+      url,
+      requestOptions,
+      apiName: this.apiName,
+      operationId: 'getSendingQueuesStatus',
+    });
+
+    return transformSendingQueuesStatusResponseIntoClientResponse(apiResponse);
+  }
+
+  /**
+   * Send an email
+   * Pass the components of the messages such as To, From, Subject, HTML and text parts, attachments, etc. Mailgun will build a MIME representation of the message and send it. Note: In order to send you must provide one of the following parameters: 'text', 'html', 'amp-html' or 'template'
+   * @param { string } domainName - Domain name used to send the message
+   * @param { SendEmailRequest } request - The data to provide to the API call.
+   */
+  public async sendEmail(domainName: string, request: SendEmailRequest): Promise<SendEmailResponse> {
+    this.client = this.getSinchClient();
+    const getParams = this.client.extractQueryParams<SendEmailRequest>(request, [] as never[]);
+    const headers: { [key: string]: string | undefined } = {
+      Accept: 'application/json',
+    };
+    const body: RequestBody = transformSendEmailRequestIntoApiRequestBody(request);
+    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/${domainName}/messages`;
+
+    const requestOptions = await this.client.prepareOptions(basePathUrl, 'POST', getParams, headers, body || undefined);
+    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
+
+    const apiResponse = await this.client.processCall<SendEmailResponseFromApi>({
+      url,
+      requestOptions,
+      apiName: this.apiName,
+      operationId: 'sendEmail',
+    });
+
+    return transformSendEmailResponseIntoClientResponse(apiResponse);
+  }
+
+  /**
+   * Send an email in MIME format
+   * Build a MIME string yourself using a MIME library for your programming language and submit it to Mailgun.
+   * @param { string } domainName - Domain name used to send the message
+   * @param { SendMimeEmailRequest } request - The data to provide to the API call.
+   */
+  public async sendMimeEmail(domainName: string, request: SendMimeEmailRequest): Promise<SendEmailResponse> {
+    this.client = this.getSinchClient();
+    const getParams = this.client.extractQueryParams<SendMimeEmailRequest>(request, [] as never[]);
+    const headers: { [key: string]: string | undefined } = {
+      'Content-Type': 'multipart/form-data',
+      Accept: 'application/json',
+    };
+    const body: RequestBody = transformSendMimeEmailRequestIntoApiRequestBody(request);
+    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/${domainName}/messages.mime`;
+
+    const requestOptions = await this.client.prepareOptions(basePathUrl, 'POST', getParams, headers, body || undefined);
+    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
+
+    const apiResponse = await this.client.processCall<SendEmailResponseFromApi>({
+      url,
+      requestOptions,
+      apiName: this.apiName,
+      operationId: 'sendMimeEmail',
+    });
+
+    return transformSendEmailResponseIntoClientResponse(apiResponse);
+  }
+
+
+  // //
+
+  /**
+ * Delete scheduled and undelivered mail
+ * Deletes all scheduled and undelivered mail from the domain queue.  This endpoint must be called on the storage API host and in the domain's region.  e.g. https://storage-us-east4.api.mailgun.net/v3/example.com/envelopes
+ *
+ * The storage hosts are `storage-us-east4.api.mailgun.net`, `storage-us-west1.api.mailgun.net`, and `storage-europe-west1.api.mailgun.net`.
+ * @param { string } domainName - The name of the domain you want to delete envelope from
+ * @param { MailgunStorageHostname } storageHostname - The storage hostname to be purged
+ */
   public async purgeSendingQueue(
     domainName: string,
     storageHostname: MailgunStorageHostname,
@@ -176,34 +186,6 @@ export class EmailsApi extends MailgunDomainApi {
     });
 
     return {};
-  }
-
-  /**
-   * Get messages queue status
-   * Provides default and scheduled message queue information.
-   * @param { string } name - The name of the domain you want get sending queues from
-   */
-  public async getSendingQueuesStatus(name: string): Promise<SendingQueuesStatusResponse> {
-    this.client = this.getSinchClient();
-    const getParams = {};
-    const headers: { [key: string]: string | undefined } = {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    };
-    const body: RequestBody = '';
-    const basePathUrl = `${this.client.apiClientOptions.hostname}/v3/domains/${name}/sending_queues`;
-
-    const requestOptions = await this.client.prepareOptions(basePathUrl, 'GET', getParams, headers, body || undefined);
-    const url = this.client.prepareUrl(requestOptions.hostname, requestOptions.queryParams);
-
-    const apiResponse = await this.client.processCall<SendingQueuesStatusResponseFromApi>({
-      url,
-      requestOptions,
-      apiName: this.apiName,
-      operationId: 'getSendingQueuesStatus',
-    });
-
-    return transformSendingQueuesStatusResponseIntoClientResponse(apiResponse);
   }
 
 }
