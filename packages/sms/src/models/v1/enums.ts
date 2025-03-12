a1cfdb6117a55447884f4698d0cbd010df706e47
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-03-12 22:42:57 +0100

    DEVEXP-752 [SMS] Update Delivery Reports models

--- packages/sms/src/models/v1/enums.ts
+++ packages/sms/src/models/v1/enums.ts
@@ -1,3 +1,6 @@
+/**
+ * Kind of delivery report
+ */
 export type DeliveryReportEnum =
   'none'
   | 'summary'
@@ -5,7 +8,10 @@ export type DeliveryReportEnum =
   | 'per_recipient'
   | 'per_recipient_final';
 
-export type DeliveryReportStatusEnum =
+/**
+ * The simplified status as described in _Delivery Report Statuses_.
+ */
+export type DeliveryStatusEnum =
   'Queued'
   | 'Dispatched'
   | 'Aborted'
@@ -16,3 +22,14 @@ export type DeliveryReportStatusEnum =
   | 'Failed'
   | 'Expired'
   | 'Unknown';
+
+/** @deprecated */
+export type DeliveryReportStatusEnum = DeliveryReportEnum;
+
+/**
+ * The delivery report status code provides a more detailed view of what happened with a message. The REST API error codes are a combination of [SMPP error codes](/docs/sms/smpp/error-codes/#status-reports-error-codes), [MMS error codes](/docs/mms/7-service/mms-status-codes/) and custom codes.
+ */
+export type DeliveryReceiptCodeEnum = 0 | 400 | 401
+  | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 410
+  | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | number;
+
