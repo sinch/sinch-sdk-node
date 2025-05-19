353e40284eec460578e6414b788ddce0b23497b4
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-05-19 17:57:30 +0200

    DEVEXP-883: (Voice) Sync OAS - Week 18 (#165)

--- packages/voice/src/models/v1/helper.ts
+++ packages/voice/src/models/v1/helper.ts
@@ -218,6 +218,7 @@ export const iceInstructionHelper = {
   sendDtmf: svamlInstructionHelper.buildSendDtmf,
   setCookie: svamlInstructionHelper.buildSetCookie,
   startRecording: svamlInstructionHelper.buildStartRecording,
+  stopRecording: svamlInstructionHelper.buildStopRecording,
   answer: svamlInstructionHelper.buildAnswer,
 };
 
@@ -225,6 +226,7 @@ export const pieActionHelper = {
   hangup: svamlActionHelper.buildHangup,
   continue: svamlActionHelper.buildContinue,
   connectConf: svamlActionHelper.buildConnectConf,
+  connectPstn: svamlActionHelper.buildConnectPstn,
   connectSip: svamlActionHelper.buildConnectSip,
   runMenu: svamlActionHelper.buildRunMenu,
 };
