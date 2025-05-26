d589dacf00ab7be1fdea4a30b88a3f689ff6909d
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-05-26 18:09:55 +0200

    Fix comments

--- packages/numbers/src/models/v1/voice-configuration/voice-configuration.ts
+++ packages/numbers/src/models/v1/voice-configuration/voice-configuration.ts
@@ -8,21 +8,21 @@ export type VoiceConfiguration = VoiceConfigurationRtc | VoiceConfigurationFax |
 export interface VoiceConfigurationRtc {
   /** The voice application type. */
   type?: 'RTC'
-  /** Your app ID for the Voice API. The `appId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/voice/apps) under Voice, then apps. */
+  /** Your app ID for the Voice API. The `appId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/voice/apps). */
   appId?: string;
 }
 
 export interface VoiceConfigurationFax {
   /** The voice application type. */
   type: 'FAX'
-  /** The service ID for the FAX configuration. The `serviceId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/fax/services) under fax, then services.*/
+  /** The service ID for the FAX configuration. The `serviceId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/fax/services).*/
   serviceId?: string;
 }
 
 export interface VoiceConfigurationEst {
   /** The voice application type. */
   type: 'EST'
-  /** The trunk ID for the EST voice configuration. The `trunkId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/trunks/your-trunks) under sip, then trunks.*/
+  /** The trunk ID for the EST voice configuration. The `trunkId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/trunks/your-trunks). */
   trunkId?: string;
 }
 
@@ -30,7 +30,7 @@ export interface VoiceConfigurationEst {
  * The current voice configuration for this number. During scheduled provisioning, the app ID value may be empty in a response if it is still processing or if it has failed. The status of scheduled provisioning will show under a `scheduledVoiceProvisioning` object if it\'s still running. Once processed successfully, the `appId` sent will appear directly under the `voiceConfiguration` object.
  */
 export interface VoiceConfigurationResponse {
-  /** Your app ID for the Voice API. The `appId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/voice/apps) under Voice, then apps. */
+  /** Your app ID for the Voice API. The `appId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/voice/apps). */
   appId?: string;
   /** Timestamp when the status was last updated. */
   lastUpdatedTime?: Date | null;
@@ -38,8 +38,8 @@ export interface VoiceConfigurationResponse {
   scheduledVoiceProvisioning?: ScheduledVoiceProvisioning | null;
   /** The type of voice configuration. Default is `RTC`. */
   type: 'RTC' | 'EST' | 'FAX';
-  /** The trunk ID for the EST voice configuration. The `trunkId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/trunks/your-trunks) under sip, then trunks.*/
+  /** The trunk ID for the EST voice configuration. The `trunkId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/trunks/your-trunks). */
   trunkId?: string;
-  /** The service ID for the FAX configuration. The `serviceId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/fax/services) under fax, then services.*/
+  /** The service ID for the FAX configuration. The `serviceId` can be found in your [Sinch Customer Dashboard](https://dashboard.sinch.com/fax/services). */
   serviceId?: string;
 }
