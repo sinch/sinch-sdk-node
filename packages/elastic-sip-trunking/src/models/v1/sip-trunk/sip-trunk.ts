c1992b0bf34d6fe5c5b6741d68a4e8352cf61170
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2025-05-26 17:58:53 +0200

    DEVEXP-902: (EST) OAS sync - Week 20

--- packages/elastic-sip-trunking/src/models/v1/sip-trunk/sip-trunk.ts
+++ packages/elastic-sip-trunking/src/models/v1/sip-trunk/sip-trunk.ts
@@ -1,9 +1,7 @@
-
 /**
  * A created SIP trunk.
  */
 export interface SipTrunk {
-
   /** The SIP trunk id. */
   id?: string;
   /** The host of the domain you would like to have for you trunk. */
@@ -18,6 +16,11 @@ export interface SipTrunk {
   callsPerSecond?: number;
   /** Enable caller name lookup for incoming calls. US and canada only. */
   enableCallerName?: boolean;
+  /** The valid E.164 phone number that will be dialed on inbound calls when no SIP endpoints are available. Required if `enableCallForward` property is `true`. */
+  callForwardNumber?: string;
+  /** When enabled, if an inbound call can not be delivered to a SIP endpoint, calls will be forwarded to the configured `callForwardNumber`. To forward all calls without trying SIP, simply disable (or delete) all SIP endpoints on this trunk. Logs (and billing) will be created for 2 calls (inbound and outbound).
+   * **Note:** Since there is no SIP connection, PCAPs will not be generated. */
+  enableCallForward?: boolean;
   /** The date and time that the SIP trunk was created. */
   createTime?: Date;
   /** The date and time that the SIP trunk was last modified. */
