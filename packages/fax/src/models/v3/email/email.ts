80b9980ad2bdc6eaaeb9342089556ff96a81cd56
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2026-03-30 17:59:11 +0200

    Small Fax API fixes

--- packages/fax/src/models/v3/email/email.ts
+++ packages/fax/src/models/v3/email/email.ts
@@ -11,4 +11,8 @@ export interface EmailRequest extends EmailBase {}
 export interface Email extends EmailBase {
   /** The `Id` of the project associated with the call. */
   projectId?: string;
+  /** The date and time the email was created. */
+  createdAt?: Date;
+  /** The date and time the email was updated. */
+  updatedAt?: Date;
 }
--- packages/fax/src/models/v3/service-phone-number/service-phone-number.ts
+++ packages/fax/src/models/v3/service-phone-number/service-phone-number.ts
@@ -1,3 +1,5 @@
+import { Permissions } from '../enums';
+
 export interface ServicePhoneNumber {
   /** A phone number in [E.164](https://community.sinch.com/t5/Glossary/E-164/ta-p/7537) format, including the leading '+'. */
   phoneNumber?: string;
@@ -5,4 +7,6 @@ export interface ServicePhoneNumber {
   projectId?: string;
   /** ID of the fax service used. */
   serviceId?: string;
+  /** Allows you to set permissions for sending and receiving faxes to this email/phone number combination. Default value is `both`. */
+  permissions?: Permissions;
 }
