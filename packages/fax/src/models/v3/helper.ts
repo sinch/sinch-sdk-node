bef88bebc4f27ec14c7775972f729b8641730a20
Hugo Rivas Geslin <smaartproductions@gmail.com>
2026-06-17 18:20:07 +0200

    refactor(fax): simplify convertToSupportedFileType by removing logger and warning messages

--- packages/fax/src/models/v3/helper.ts
+++ packages/fax/src/models/v3/helper.ts
@@ -1,17 +1,10 @@
-import { FaxBase64FileType, validBase64FileTypes } from './enums';
-import { Logger, SinchLogger, resolveLogger } from '@sinch/sdk-client';
+import { FaxBase64FileType } from './enums';
 
 export const convertToSupportedFileType = (
   fileType: string | undefined,
-  logger?: Logger | null,
 ): FaxBase64FileType | undefined => {
-  const sinchLogger = new SinchLogger(resolveLogger(logger));
   if (!fileType) {
-    sinchLogger.warn('No file extension has been defined.');
     return undefined;
-  }
-  if (!validBase64FileTypes.includes(fileType.toUpperCase() as FaxBase64FileType)) {
-    sinchLogger.warn(`The file extension "${fileType.toUpperCase()}" is not supported.`);
   }
   return fileType.toUpperCase() as FaxBase64FileType;
 };
